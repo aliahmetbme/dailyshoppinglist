0e53480f563fc2dc0a155fe680b33145d73f4fe7
Ali ahmet Erdoğdu <aliahmeterdogdu@192.168.1.105>
2024-06-21 16:44:47 +0300

    first commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Ali ahmet Erdoğdu on 16.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var AddButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        AddButton.makeButtonCircular()
+
+    }
+}
+/*
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+ */
+extension UIButton {
+    func makeButtonCircular () {
+        self.layer.cornerRadius = self.frame.size.width / 2
+        self.clipsToBounds = true
+
+    }
+
+}
